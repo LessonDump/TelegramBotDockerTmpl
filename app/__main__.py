2537217d448a4b900292dc59dc28ba315b152235
Aleksey Voko <voko.aleksey@gmail.com>
2023-03-25 08:14:09 +0400

    fix(app): fix logging

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,6 @@
 import logging
+from datetime import datetime
+from pathlib import Path
 
 from aiogram.utils.executor import start_polling
 
@@ -6,9 +8,12 @@ from app.handlers import *
 
 
 def main():
+    log_name = f'logs/{datetime.now().strftime("%Y-%m-%d")}.log'
+    Path(log_name).parent.mkdir(parents=True, exist_ok=True)
+
     logging.basicConfig(
         level=logging.INFO,
-        filename="app_log.log",
+        filename=log_name,
         filemode="a"
     )
 
